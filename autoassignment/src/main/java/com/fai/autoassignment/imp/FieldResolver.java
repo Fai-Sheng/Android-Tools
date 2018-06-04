cbb8db0a3759e3197ae60c82f2fc1ce7d6f01f1e
Fai-Sheng <769273297@qq.com>
2018-06-04 23:56:40 +0800

    思考 一个自动 赋值工具2

--- autoassignment/src/main/java/com/fai/autoassignment/imp/FieldResolver.java
+++ autoassignment/src/main/java/com/fai/autoassignment/imp/FieldResolver.java
@@ -2,6 +2,8 @@ package com.fai.autoassignment.imp;
 
 import com.fai.autoassignment.core.Resolver;
 
+import java.lang.reflect.Field;
+
 /**
  * Created by PVer on 2018/6/4.
  */
@@ -21,4 +23,26 @@ public class FieldResolver implements Resolver{
 
     // 4.存在内部类对象 的情况如何处理
     // 5.内部类的对象 的值 赋值给一个goal的普通值
+
+    // 6.多重内部类 对象（特殊情况）
+
+    // 7.数组和对象如何判断，String类型的判断
+
+
+    private<K> void resolveGoalObj(K goal) {
+        if(goal == null)
+        {
+            return;
+        }
+
+        Field[] fields = goal.getClass().getDeclaredFields();
+
+        //判断是否有内部类
+
+    }
+
+
+    private<T> void resolveSrcObj(T goal){
+
+    }
 }
